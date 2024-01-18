31a6138a898b0f4b324d9aac94c8adddad4886c7
Patrick C Davis <patrickdavis@microsoft.com>
2024-01-18 20:09:27 +0000

    update titles

--- app-ui/web/src/pages/BlankPage.tsx
+++ app-ui/web/src/pages/BlankPage.tsx
@@ -13,7 +13,15 @@ import { ModelDropDown } from "../components/ModelDropDown/ModelDropDown";
 import React from "react";
 import { FileDropDown } from "../components/FileDropDown/FileDropDown";
 import { PromptDropDown } from "../components/PromptDropDown/PromptDropDown";
-import { DefaultButton, PrimaryButton, Slider, Spinner, SpinnerSize, Stack, Text } from "@fluentui/react";
+import {
+  DefaultButton,
+  PrimaryButton,
+  Slider,
+  Spinner,
+  SpinnerSize,
+  Stack,
+  Text,
+} from "@fluentui/react";
 import { AOAIResults } from "../components/AOAIResult/AOAIResult";
 
 export default function blankPage() {
@@ -59,7 +67,7 @@ export default function blankPage() {
         const res = await getAOAIResponse(
           selectedFile,
           selectedDeployment,
-          selMessages, 
+          selMessages,
           docLength,
           maxTokens,
           temperature,
@@ -68,8 +76,7 @@ export default function blankPage() {
         console.log("Chat Respose");
         console.log(res);
         setAoaiResult(res);
-      }
-      else{
+      } else {
         setAoaiResult("Please select a file, model, and prompt");
         setDisableRunIt(false);
       }
@@ -82,86 +89,167 @@ export default function blankPage() {
   const onChangeTempature = (value: number) => setTemperature(value);
   const onChangeTopP = (value: number) => setTopP(value);
 
-
   return (
     <div>
-      <h1 style={{ color: "black" }}>Azure Open AI Demo </h1>
+      <Stack>
+        <Stack.Item align="center">
+          <Text variant="mega" style={{ color: "black" }}>
+            Azure Open AI Demo
+          </Text>
+        </Stack.Item>
+      </Stack>
       <Stack tokens={{ childrenGap: 10, padding: 25 }}>
         <Stack.Item>
-      <h2 style={{ color: "black" }}>
-        1){" "}
-        {selectedFile ? (
-          <span>File Selected: {selectedFile}</span>
-        ) : (
-          " Pick a File"
-        )}
-      </h2>
-      <FileDropDown fileList={fileList} setFile={setSelectedFile} />
-      </Stack.Item>
-      <Stack.Item>
-      <h2 style={{ color: "black" }}>
-        2){" "}
-        {selectedDeployment ? (
-          <span>Model Selected: {selectedDeployment}</span>
-        ) : (
-          " Pick A Model"
-        )}
-      </h2>
-      <ModelDropDown
-        aoaiModel={deploymentListResponse}
-        setSelDep={setSelectedDeployment}
-      />
-      </Stack.Item>
-      <Stack.Item>
-      <h2 style={{ color: "black" }}>
-        3){" "}
-        {selectedPrompt ? (
-          <span>Prompt Selected: {selectedPrompt}</span>
-        ) : (
-          " Pick The Prompt"
-        )}{" "}
-      </h2>
-      <span>
-        The two keywords are 'doc' for the contents of the document selected
-        above and 'docName' for the document name. Place the variables inside
-        double currly braces.
-      </span>
-      <PromptDropDown
-        setPrompts={setSelectedPrompt}
-        setSelMessages={setSelMessages}
-        selMessages={selMessages}
-      />
-    </Stack.Item>
-    <Stack.Item>
-        <Stack tokens={{ childrenGap: 10, padding: 5 }}>
+          <h2 style={{ color: "black" }}>
+            1){" "}
+            {selectedFile ? (
+              <span>File Selected: {selectedFile}</span>
+            ) : (
+              " Pick a File"
+            )}
+          </h2>
+          <FileDropDown fileList={fileList} setFile={setSelectedFile} />
+        </Stack.Item>
+        <Stack.Item>
+          <h2 style={{ color: "black" }}>
+            2){" "}
+            {selectedDeployment ? (
+              <span>Model Selected: {selectedDeployment}</span>
+            ) : (
+              " Pick A Model"
+            )}
+          </h2>
+          <ModelDropDown
+            aoaiModel={deploymentListResponse}
+            setSelDep={setSelectedDeployment}
+          />
+        </Stack.Item>
+        <Stack.Item>
+          <h2 style={{ color: "black" }}>
+            3){" "}
+            {selectedPrompt ? (
+              <span>Prompt Selected: {selectedPrompt}</span>
+            ) : (
+              " Pick The Prompt"
+            )}{" "}
+          </h2>
+          <span>
+            The two keywords are 'doc' for the contents of the document selected
+            above and 'docName' for the document name. Place the variables
+            inside double currly braces.
+          </span>
+          <PromptDropDown
+            setPrompts={setSelectedPrompt}
+            setSelMessages={setSelMessages}
+            selMessages={selMessages}
+          />
+        </Stack.Item>
+        <Stack.Item>
+          <Stack tokens={{ childrenGap: 10, padding: 5 }}>
             <Stack.Item>
-                <h2 style={{ color: "black" }}>4) AOAI Parameters</h2>
-                </Stack.Item>
+              <h2 style={{ color: "black" }}>4) AOAI Parameters</h2>
+            </Stack.Item>
             <Stack.Item>
-      <Text>This is the maximum number of characters used in the document.</Text>
-      <Slider label="Document Cutoff Length" value={docLength} min={1000} max={100000} step={1000} defaultValue={10000} onChange={sliderOnChange} showValue snapToStep />
-      </Stack.Item>
-      <Stack.Item>
-      <Text>The maximum number of tokens to generate. Requests can use up to 4096 tokens shared between the prompt and completion.</Text>
-      <Slider label="Max Tokens" value={maxTokens} min={100} max={32000} step={100} defaultValue={4000} onChange={onChangeMaxTokens} showValue snapToStep />
-      </Stack.Item>
-      <Stack.Item>
-      <Text>Controls the randomness of the output. A higher value (closer to 1) makes the output more random, while a lower value (closer to 0) makes it more deterministic.</Text>
-      <Slider label="Temperature" value={temperature} min={0.0} max={1} step={0.1} defaultValue={0.5} onChange={onChangeTempature} showValue snapToStep />
-      </Stack.Item>
-      <Stack.Item>
-      <Text>An alternative to temperature for controlling randomness. It specifies the cumulative probability cutoff for token selection. Only tokens with cumulative probability less than top_p are considered for selection.</Text>
-      <Slider label="Top P" value={topP} min={0} max={1} step={0.1} defaultValue={0} onChange={onChangeTopP} showValue snapToStep />
-      </Stack.Item>
-      </Stack >
-      </Stack.Item>
-      <Stack.Item>
-      <h2 style={{ color: "black" }}>Result: </h2>
-      {aoaiResult ? <AOAIResults res={aoaiResult}/> : <></>}
-      { !aoaiResult && disableRunIt ? <Spinner size={SpinnerSize.large} /> : <></> }
-      { aoaiResult ? <></> :<PrimaryButton disabled={disableRunIt} text="Run it!" onClick={runAOAI} />}
-      { aoaiResult ? <DefaultButton text="Clear" onClick={() => {setAoaiResult(undefined); setDisableRunIt(false);}} /> : <></>}
-      </Stack.Item>
+              <Text>
+                This is the maximum number of characters used in the document.
+              </Text>
+              <Slider
+                label="Document Cutoff Length"
+                value={docLength}
+                min={1000}
+                max={100000}
+                step={1000}
+                defaultValue={10000}
+                onChange={sliderOnChange}
+                showValue
+                snapToStep
+              />
+            </Stack.Item>
+            <Stack.Item>
+              <Text>
+                The maximum number of tokens to generate. Requests can use up to
+                4096 tokens shared between the prompt and completion.
+              </Text>
+              <Slider
+                label="Max Tokens"
+                value={maxTokens}
+                min={100}
+                max={32000}
+                step={100}
+                defaultValue={4000}
+                onChange={onChangeMaxTokens}
+                showValue
+                snapToStep
+              />
+            </Stack.Item>
+            <Stack.Item>
+              <Text>
+                Controls the randomness of the output. A higher value (closer to
+                1) makes the output more random, while a lower value (closer to
+                0) makes it more deterministic.
+              </Text>
+              <Slider
+                label="Temperature"
+                value={temperature}
+                min={0.0}
+                max={1}
+                step={0.1}
+                defaultValue={0.5}
+                onChange={onChangeTempature}
+                showValue
+                snapToStep
+              />
+            </Stack.Item>
+            <Stack.Item>
+              <Text>
+                An alternative to temperature for controlling randomness. It
+                specifies the cumulative probability cutoff for token selection.
+                Only tokens with cumulative probability less than top_p are
+                considered for selection.
+              </Text>
+              <Slider
+                label="Top P"
+                value={topP}
+                min={0}
+                max={1}
+                step={0.1}
+                defaultValue={0}
+                onChange={onChangeTopP}
+                showValue
+                snapToStep
+              />
+            </Stack.Item>
+          </Stack>
+        </Stack.Item>
+        <Stack.Item>
+          <h2 style={{ color: "black" }}>Result: </h2>
+          {aoaiResult ? <AOAIResults res={aoaiResult} /> : <></>}
+          {!aoaiResult && disableRunIt ? (
+            <Spinner size={SpinnerSize.large} />
+          ) : (
+            <></>
+          )}
+          {aoaiResult ? (
+            <></>
+          ) : (
+            <PrimaryButton
+              disabled={disableRunIt}
+              text="Run it!"
+              onClick={runAOAI}
+            />
+          )}
+          {aoaiResult ? (
+            <DefaultButton
+              text="Clear"
+              onClick={() => {
+                setAoaiResult(undefined);
+                setDisableRunIt(false);
+              }}
+            />
+          ) : (
+            <></>
+          )}
+        </Stack.Item>
       </Stack>
     </div>
   );
